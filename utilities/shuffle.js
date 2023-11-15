0bdbed544f011c456bfbcdd9831e013f371ee864
phone_thet_naing <phonethetnaing34@gmail.com>
2023-11-15 22:38:19 +0630

    image to link

--- utilities/shuffle.js
+++ utilities/shuffle.js
@@ -1,13 +1,18 @@
+// import path from 'path';
+
 function shuffle() {
 	const assets = [
 		// { image: '/src/assets/angular.png' },
-		{ image: '/src/assets/javascript.png' },
-		{ image: '/src/assets/nextjs.png' },
-		{ image: '/src/assets/node.png' },
-		{ image: '/src/assets/react.png' },
-		{ image: '/src/assets/typescript.png' },
-		{ image: '/src/assets/vite.png' },
-		{ image: '/src/assets/vue.png' },
+		{
+			image:
+				'https://github.com/phone-thet-naing/memory-game/blob/master/src/assets/javascript.png?raw=true',
+		},
+		{ image: 'https://github.com/phone-thet-naing/memory-game/blob/master/src/assets/nextjs.png?raw=true' },
+		{ image: 'https://github.com/phone-thet-naing/memory-game/blob/master/src/assets/node.png?raw=true' },
+		{ image: 'https://github.com/phone-thet-naing/memory-game/blob/master/src/assets/react.png?raw=true' },
+		{ image: 'https://github.com/phone-thet-naing/memory-game/blob/master/src/assets/typescript.png?raw=true' },
+		{ image: 'https://github.com/phone-thet-naing/memory-game/blob/master/src/assets/vite.png?raw=true' },
+		{ image: 'https://github.com/phone-thet-naing/memory-game/blob/master/src/assets/vue.png?raw=true' },
 		{
 			image:
 				'https://github.com/phone-thet-naing/memory-game/blob/master/src/assets/angular.png?raw=true',
