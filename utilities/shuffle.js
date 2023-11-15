78f240f1000d0b934e013231d591ec1b2443d10c
phone_thet_naing <phonethetnaing34@gmail.com>
2023-11-15 22:17:11 +0630

    change to link

--- utilities/shuffle.js
+++ utilities/shuffle.js
@@ -1,6 +1,6 @@
 function shuffle() {
 	const assets = [
-		{ image: '/src/assets/angular.png' },
+		// { image: '/src/assets/angular.png' },
 		{ image: '/src/assets/javascript.png' },
 		{ image: '/src/assets/nextjs.png' },
 		{ image: '/src/assets/node.png' },
@@ -8,11 +8,20 @@ function shuffle() {
 		{ image: '/src/assets/typescript.png' },
 		{ image: '/src/assets/vite.png' },
 		{ image: '/src/assets/vue.png' },
+		{
+			image:
+				'https://github.com/phone-thet-naing/memory-game/blob/master/src/assets/angular.png?raw=true',
+		},
 	];
 
 	return [...assets, ...assets]
 		.sort(() => Math.random() - 0.5)
-		.map((card) => ({ ...card, id: Math.random(), isSelected: false, alreadySelected: false }));
+		.map((card) => ({
+			...card,
+			id: Math.random(),
+			isSelected: false,
+			alreadySelected: false,
+		}));
 }
 
 export default shuffle;
